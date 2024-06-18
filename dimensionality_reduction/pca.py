84a9e1413aa324e3e875efb3b931d7a2e523b9d9
user666acid <101744246+user666acid@users.noreply.github.com>
2024-06-18 13:49:37 +0300

    Update pca.py

--- dimensionality_reduction/pca.py
+++ dimensionality_reduction/pca.py
@@ -1,1 +1,48 @@
+class MyPCA():
+    """ Класс для работы с методом главных компонент. """
+    
+    def __init__(self, n_components: int = 3) -> None:
+        """ Конструктор для экземпляра класса MyPca. 
 
+        Args:
+            n_components (int): Количество главных компонент.
+
+        Returns:
+            None
+
+        """
+        self.n_components = n_components
+    
+    def fit_transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """ Выполняет снижение размерности исходного датасета.
+
+        Args:
+            X (pd.DataFrame): Исходный датасет.
+
+        Returns:
+            pd.DataFrame: Преобразованный датасет пониженной размерности.
+
+        """
+        X_centered = X - np.mean(X, axis=0)
+        cov_matrix = np.cov(X_centered, rowvar=False)
+        
+        values, vectors = np.linalg.eigh(cov_matrix)  
+        idxs = np.argsort(values)[-self.n_components:][::-1]
+        principal_components = vectors[:, idxs]
+        
+        X_transformed = X_centered @ principal_components
+         
+        return X_transformed
+    
+    def __repr__(self) -> str:
+        """ Выводит текстовое представление объекта класса.
+
+        Returns:
+            str: Строка с текстовым представлением объекта класса.
+
+        """
+        params = []
+        for k, v in self.__dict__.items():
+            params.append(f'{k}={v}')
+            
+        return f'{type(self).__name__} class: {", ".join(params)}'
